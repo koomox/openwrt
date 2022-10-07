1700d07ed2b4bec1948fb16bb7b4c49a39ac4fbc
allen.w <koomoxs@gmail.com>
2022-10-07 13:15:35 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Copyright (c) 2021-2025
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+git clone --depth=1 https://github.com/kenzok8/small-package.git
