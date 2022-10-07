5023e2d7507bdba12c0ea534c4dd254630f17a22
allen.w <koomoxs@gmail.com>
2022-10-07 14:24:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,8 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.230/g' package/base-files/files/bin/config_generate
-sed -i "113i\\\t\t\t\tset network.\$1.dns='127.0.0.1 119.29.29.29 1.1.1.1'" package/base-files/files/bin/config_generate
-sed -i "113i\\\t\t\t\tset network.\$1.gateway='192.168.0.1'" package/base-files/files/bin/config_generate
-sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
-sed -i '/CST-8/a \\t\tset system\.\@system\[\-1\]\.zonename=\'Asia\/Shanghai\' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.230/g' openwrt/package/base-files/files/bin/config_generate
+sed -i "113i\\\t\t\t\tset network.\$1.dns='127.0.0.1 119.29.29.29 1.1.1.1'" openwrt/package/base-files/files/bin/config_generate
+sed -i "113i\\\t\t\t\tset network.\$1.gateway='192.168.0.1'" openwrt/package/base-files/files/bin/config_generate
+sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
+sed -i '/CST-8/a \\t\tset system\.\@system\[\-1\]\.zonename=\'Asia\/Shanghai\' openwrt/package/base-files/files/bin/config_generate
