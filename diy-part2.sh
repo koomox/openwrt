5489901296f7546e90deed13e13ae3aa5ad906bf
allen.w <koomoxs@gmail.com>
2022-10-08 21:11:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.230/g' openwrt/package/base-files/files/bin/config_generate
-sed -i "set network.\$1.dns='127.0.0.1 119.29.29.29 1.1.1.1'" openwrt/package/base-files/files/bin/config_generate
-sed -i "set network.\$1.gateway='192.168.0.1'" openwrt/package/base-files/files/bin/config_generate
+# sed -i "set network.\$1.dns='127.0.0.1 119.29.29.29 1.1.1.1'" openwrt/package/base-files/files/bin/config_generate
+# sed -i "set network.\$1.gateway='192.168.0.1'" openwrt/package/base-files/files/bin/config_generate
 sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
 sed -i '/CST-8/a \\t\tset system\.\@system\[\-1\]\.zonename=\'Asia\/Shanghai\' openwrt/package/base-files/files/bin/config_generate
