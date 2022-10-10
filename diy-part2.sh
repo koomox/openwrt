686204e9ea9190e21dbc8e8ba8b73cbb7d31be8a
allen.w <koomoxs@gmail.com>
2022-10-10 10:24:15 +0800

    commit

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,5 @@
 sed -i 's/192.168.1.1/192.168.0.230/g' openwrt/package/base-files/files/bin/config_generate
 # sed -i "set network.\$1.dns='127.0.0.1 119.29.29.29 1.1.1.1'" openwrt/package/base-files/files/bin/config_generate
 # sed -i "set network.\$1.gateway='192.168.0.1'" openwrt/package/base-files/files/bin/config_generate
-sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
-sed -i '/CST-8/a \\t\tset system\.\@system\[\-1\]\.zonename=\'Asia\/Shanghai\' openwrt/package/base-files/files/bin/config_generate
+# sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
+# sed -i '/CST-8/a \\t\tset system\.\@system\[\-1\]\.zonename=\'Asia\/Shanghai\' openwrt/package/base-files/files/bin/config_generate
